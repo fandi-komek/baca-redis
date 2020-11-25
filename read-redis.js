eb15875cf131b1370828d26843b51acd06c0fd9c
Fandi <komekbtn@gmail.com>
2020-11-25 22:29:10 +0700

    Sampling 1 redis.keys

--- read-redis.js
+++ read-redis.js
@@ -1,29 +1,23 @@
-const { rejects } = require("assert");
-const { resolve } = require("path");
 const redis_1 = require("redis");
-const util_1 = require("util");
 
 const redisBroadcaster  = redis_1.createClient('6379','192.168.99.24');
 const campaignList      = ['COBAIN','TEST1','TEST2','TEST3', 'TEST4'];
 function getCampaignIsActive(campaign){
     return new Promise((resolve, reject) => {
-        if (!campaign){
-            reject("Empty campaign");
-            return;
-        }
-        /*
-        let tmpValues = new Promise((resolve, reject) => {
-            redisBroadcaster.keys(`*|${campaign}`,(error, channels) => {
-                console.log('asli:', channels.length);
-                resolve(channels.length);
+       if (campaign !== ""){
+            let retreivedCampaignData;
+            retreivedCampaignData = new Promise((resolve, reject) => {
+                redisBroadcaster.keys(`*|${campaign}`, (error, channels) => {
+                    resolve(channels.length);
+                })
             });
-        });
-        resolve(tmpValues); 
-        */
-        redisBroadcaster.keys(`*|${campaign}`,(error, channels) => {
-            console.log('asli:', channels.length);
-            resolve(channels.length);
-        });
+            Promise.resolve(retreivedCampaignData)
+            .then((values) => {
+                resolve(values);
+            });
+       } else {
+           reject(`Empty campaign !`);
+       }
     })
 }
 
@@ -32,9 +26,8 @@ for ( var x = 0; x < campaignList.length; x++){
     let tmpHasilCampaign = 9999;
     getCampaignIsActive(tmpNamaCampaign)
     .then((values) => {
-        console.info('didalem:', values);
+        //console.info('didalem:', values);
         tmpHasilCampaign = values;
+        console.log(`Hasil campaign [${tmpNamaCampaign}] = [${tmpHasilCampaign}]`);
     }).catch((err) => console.error(err));
-    
-    console.log(`Hasil campaign [${tmpNamaCampaign}] = [${tmpHasilCampaign}]`);
 }
