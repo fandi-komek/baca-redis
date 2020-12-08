ea346dc7316abf5521ff92ff1fe75eb192cb9bcb
Fandi <komekbtn@gmail.com>
2020-12-08 12:39:17 +0700

    Nampilin data uncontacted & contacted di campaign

--- src/test-query.js
+++ src/test-query.js
@@ -3,6 +3,19 @@
  * DCCS - TAMARISK
  * AUTODIALER CAMPAIGNs
  */
+const pg_1      = require("pg");
+const redis_1   = require("redis");
+const util_1    = require("util");
+const moment_1  = require("moment");
+
+const db = new pg_1.Pool({
+    user: 'obelisk',
+    host: '192.168.99.24',
+    port: 5432,
+    database: 'obelisk',
+    password: '0b3l15k'
+});
+
 let testQuery_001   = "SELECT name, begin_time, finish_time FROM campaign WHERE name IN (SELECT DISTINCT(campaign) FROM contact WHERE username IN (SELECT member FROM groups WHERE leader='OMSPV001') GROUP BY campaign)";
 let testQuery_002   = "SELECT DISTINCT(campaign) AS campaign_name, (CASE WHEN disposition is null THEN 'NOT CONTACTED' ELSE 'CONTACTED' END) as disposition, count (customer_id) as banyak from contact WHERE username IN (SELECT member FROM groups WHERE leader='OMSPV001') group by campaign, disposition"
 let testQuery_003   = `SELECT 
@@ -89,3 +102,59 @@ GROUP BY campaign
 WHERE b.campaign_name=a.name
 ORDER BY a.name`;
 
+function getDataCampaign() {
+    return new Promise((resolve, reject) => {
+        let tempQuery_001 = testQuery_001 + " ORDER BY name";
+        db.query(tempQuery_001)
+        .then((rowDataCampaign) => {
+            if (rowDataCampaign.rowCount === 0){
+                reject('Tidak ada data ...');
+            } else {
+                let retrievedDataCampaign = [];
+                const hashDataCampaign = util_1.promisify(db.query)
+                for (let rowsCampaign of rowDataCampaign.rows){
+                    retrievedDataCampaign.push(
+                        new Promise((resolve, reject) => {
+                            hashDataCampaign.bind(db)(`SELECT COUNT(disposition) AS contacted, (COUNT(disposition is not null) - COUNT(disposition))::bigint AS not_contacted, COUNT(disposition is not null) as total_data FROM contact WHERE campaign=$1`,[rowsCampaign.name])
+                            .then((campaignCounter) => {
+                                if (campaignCounter.rowCount === 1){
+                                    resolve({
+                                        campaign: `${rowsCampaign.name}`,
+                                        beginTime: `${rowsCampaign.begin_time}`,
+                                        finishTime: `${rowsCampaign.finish_time}`,
+                                        dataCall: parseInt(campaignCounter.rows[0].total_data),
+                                        dataContacted: parseInt(campaignCounter.rows[0].contacted),
+                                        dataUncontacted: parseInt(campaignCounter.rows[0].not_contacted)
+                                    });
+                                } else {
+                                    reject(`Data not found`);
+                                }
+                            }).catch((err) => {console.error(err)})
+                        })
+                    )
+                }
+
+                Promise.all(retrievedDataCampaign)
+                .then((data) => {
+                    resolve({
+                        success: true,
+                        message: `Campaign data sebanyak ${rowDataCampaign.rowCount}`,
+                        data: data
+                    })
+                }).catch((err) => {
+                    console.error(err);
+                });
+            }
+        })
+        .catch((err) => console.error(err))
+    });
+}
+
+
+
+getDataCampaign()
+.then((message) => {
+    console.info(`Hasil eksekusi getDataCampaign:`, message);
+}).catch((error) => {
+    console.error(error);
+})
